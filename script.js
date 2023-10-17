5c137cabe2a00f3c185aaafa9588c840805bca9d
Harsh Shukla <134812055+harshaa2612@users.noreply.github.com>
2023-10-17 23:19:39 +0530

    script.js

--- script.js
+++ script.js
@@ -24,19 +24,7 @@ async function fetchNews(query) {
     }
 }
 
-function bindData(articles) {
-    const cardsContainer = document.getElementById("cards-container");
-    const newsCardTemplate = document.getElementById("template-news-card");
 
-    cardsContainer.innerHTML = "";
-
-    articles.forEach((article) => {
-        if (!article.urlToImage) return;
-        const cardClone = newsCardTemplate.content.cloneNode(true);
-        fillDataInCard(cardClone, article);
-        cardsContainer.appendChild(cardClone);
-    });
-}
 
 function fillDataInCard(cardClone, article) {
     const newsImg = cardClone.querySelector("#news-img");
